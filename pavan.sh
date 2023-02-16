dcc7e79e34535e0f19c38ef6bc579b9ebdf77437
pavan <120022942+Iam-Pavan@users.noreply.github.com>
2023-02-16 10:27:51 +0530

    Add files via upload

---
+++ pavan.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/zsh
+echo "it is pavan"
+sleep 2
+printf "          \n"
+printf "                                                                        \n "
+	printf "    _______         __                      __                  \n"
+sleep 1  
+	printf "    ||     ||       ||       ||      ||     ||      ||\\    ||  \n"
+sleep 0.5
+	printf "    ||     ||     ||  ||      ||    ||    ||   ||   || \\   ||  \n"
+sleep 0.4
+	printf "    ||_____||   ||______||     ||  ||    ||_____||  ||  \\  ||  \n"
+sleep 0.3
+	printf "    ||          ||      ||      ||||     ||     ||  ||   \\ ||  \n"
+sleep 0.2
+	printf "    ||          ||      ||       ||      ||     ||  ||    \\||  \n"
+sleep 0.1
+	printf "                                                                           \n"
+printf "            \n"
+	printf "                                                                        \n"
+
+	echo "	======= hey ,i'm pavna ======= "
+	sleep 0.3 
+	echo " 	[2] enter your name "
+	sleep 0.2
+	echo "  [3] enter your education "
+	sleep 0.1
+	echo "  [4] enter your age "
+	sleep 2 
+	echo "               by -pavan"
+	sleep 1
+printf " loading..."
+sleep 1
+printf " > "
+sleep 0.6
+printf ">> "
+sleep 0.5
+printf ">>> "
+sleep 0.4
+printf ">>>> "
+sleep 0.3
+printf ">>>>> "
+sleep 0.2
+printf ">>>>>> "
+sleep 0.1
+printf ">>>>>>> \n"
+printf "        "  
+echo " hai, "
+sleep 1
+echo " [2] >enter your name"
+read name
+echo " [3] >enter yours education"
+read education
+echo "[4] >enter your age"
+read age
+echo "***......."
+sleep 1
+echo "*****......"
+sleep 2
+printf "you exit press < ctrl+c > \n"
+sleep 1
+echo " .......waiting for information..."
+sleep 2
+printf "                                                             \n"
+echo "[OK] hello!! $name , [OK] your study is $education , [OK] and your age is $age"
+printf "                                                                                    \n"
+getdata=$((( $RANDOM %15) +age))
+sleep 0.2
+echo "[OK]>> $name ,you will become a good programmer when you are $getdata years old"
+printf "                                                                                \n"
+sleep 2
+printf " >>>>>t"
+sleep 0.5
+printf "h"
+sleep 0.5
+printf "a"
+sleep 0.5
+printf "n"
+sleep 0.5
+printf "k"
+sleep 0.5
+printf "s"
+sleep 0.5
+printf "  \n"
+sleep 0.1
+printf " \n"
+printf " it is just for fun\n"
+sleep 1 
+
+
